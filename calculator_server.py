b582d839b58a4d0fd72f65c148e029b31be7636f
Janhavi Dahihande <janhavidahihande@gmail.com>
2018-09-07 12:20:17 -0700

    Added comments and indentation

--- calculator_server.py
+++ calculator_server.py
@@ -1,18 +1,3 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""The Python implementation of the GRPC helloworld.Greeter server."""
-
 from concurrent import futures
 import time
 
@@ -25,7 +10,7 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
 class Calculator(calculator_pb2_grpc.CalculatorServicer):
-
+    #"add" method to add the user inputs
     def add(self, request, context):
         finalValue = request.a + request.b
         return calculator_pb2.AddReply(sum='(%d + %d) = %s' % (request.a,request.b,finalValue))
